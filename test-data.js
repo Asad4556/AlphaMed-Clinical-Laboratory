84b55801a1a38c8b4277c25dab6eb168fee6e383
Asad4556 <mrasad0101@gmail.com>
2025-07-24 15:53:54 +0500

    Create test-data.js

---
+++ test-data.js
@@ -0,0 +1,50 @@
+// Global Test Data (Grouped by Section)
+const labTestData = {
+  "Hematology": [
+    { testName: "Hemoglobin", normal: "13-17 g/dL (Male), 12-15 g/dL (Female)", unit: "g/dL" },
+    { testName: "WBC", normal: "4,000-11,000 /mm³", unit: "/mm³" },
+    { testName: "Platelet Count", normal: "150,000–400,000 /mm³", unit: "/mm³" },
+    { testName: "RBC", normal: "4.5-5.9 million/mcL (Male)", unit: "million/mcL" }
+  ],
+  "Biochemistry": [
+    { testName: "Blood Glucose (Fasting)", normal: "70-100 mg/dL", unit: "mg/dL" },
+    { testName: "Blood Urea", normal: "7-20 mg/dL", unit: "mg/dL" },
+    { testName: "Serum Creatinine", normal: "0.6-1.3 mg/dL", unit: "mg/dL" },
+    { testName: "Total Cholesterol", normal: "<200 mg/dL", unit: "mg/dL" }
+  ],
+  "Liver Function Test (LFT)": [
+    { testName: "ALT (SGPT)", normal: "7-56 U/L", unit: "U/L" },
+    { testName: "AST (SGOT)", normal: "10-40 U/L", unit: "U/L" },
+    { testName: "Bilirubin Total", normal: "0.3-1.2 mg/dL", unit: "mg/dL" },
+    { testName: "Alkaline Phosphatase", normal: "44-147 IU/L", unit: "IU/L" }
+  ],
+  "Kidney Function Test (KFT)": [
+    { testName: "Serum Uric Acid", normal: "3.4-7.0 mg/dL", unit: "mg/dL" },
+    { testName: "Creatinine Clearance", normal: "88-128 mL/min", unit: "mL/min" }
+  ],
+  "Urine Examination": [
+    { testName: "pH", normal: "4.6-8.0", unit: "" },
+    { testName: "Specific Gravity", normal: "1.005-1.030", unit: "" },
+    { testName: "Protein", normal: "Negative", unit: "" },
+    { testName: "Glucose", normal: "Negative", unit: "" }
+  ],
+  "Thyroid Profile": [
+    { testName: "TSH", normal: "0.4–4.0 mIU/L", unit: "mIU/L" },
+    { testName: "Free T3", normal: "2.3–4.2 pg/mL", unit: "pg/mL" },
+    { testName: "Free T4", normal: "0.8–1.8 ng/dL", unit: "ng/dL" }
+  ],
+  "Serology": [
+    { testName: "HbsAg", normal: "Non-reactive", unit: "" },
+    { testName: "HCV Antibody", normal: "Non-reactive", unit: "" },
+    { testName: "HIV", normal: "Non-reactive", unit: "" }
+  ],
+  "COVID-19": [
+    { testName: "PCR Test", normal: "Negative", unit: "" },
+    { testName: "Rapid Antigen", normal: "Negative", unit: "" }
+  ],
+  "Others": [
+    { testName: "Vitamin D", normal: "20-50 ng/mL", unit: "ng/mL" },
+    { testName: "Vitamin B12", normal: "200-900 pg/mL", unit: "pg/mL" },
+    { testName: "Calcium", normal: "8.6-10.2 mg/dL", unit: "mg/dL" }
+  ]
+};
