191d04ffdb96a1c4aa098a771eb83f3fda6beaaa
Asad4556 <mrasad0101@gmail.com>
2025-07-30 14:00:59 +0500

    Update labSections.js

--- labSections.js
+++ labSections.js
@@ -1,1 +1,43 @@
-console.log('Lab sections loaded.');
+// labSections.js
+
+const labSections = {
+  "Pathology": [
+    "Urine R/E", "Stool R/E", "Urine Culture", "Stool Culture", "Semen Analysis"
+  ],
+  "Hematology": [
+    "CBC", "Hemoglobin", "RBC Count", "WBC Count", "Platelet Count", "ESR", "Peripheral Smear"
+  ],
+  "Biochemistry": [
+    "Glucose Fasting", "Glucose Random", "Glucose 2hr PP", "HbA1c", "Liver Function Test (LFT)",
+    "Renal Function Test (RFT)", "Lipid Profile", "Uric Acid", "Calcium", "Phosphate", "CRP"
+  ],
+  "Microbiology": [
+    "Blood Culture", "Urine Culture", "Sputum Culture", "Wound Swab Culture", "AFB Test", "Gram Stain"
+  ],
+  "Serology": [
+    "Hepatitis B (HBsAg)", "Hepatitis C (Anti-HCV)", "HIV", "VDRL", "Widal Test", "ASO Titre"
+  ],
+  "Immunology": [
+    "Rheumatoid Factor (RF)", "C-Reactive Protein (CRP)", "ANA", "Thyroid Profile", "H. Pylori IgG", "Toxoplasma IgG/IgM"
+  ],
+  "Molecular Biology": [
+    "COVID-19 PCR", "HCV PCR", "HBV PCR", "TB PCR", "DNA Extraction"
+  ],
+  "Histopathology": [
+    "Biopsy", "FNAC", "Pap Smear", "Surgical Specimen", "Bone Marrow Biopsy"
+  ],
+  "Cytogenetics": [
+    "Karyotyping", "FISH", "Chromosomal Analysis"
+  ],
+  "Hormonal Assays": [
+    "TSH", "T3", "T4", "FSH", "LH", "Prolactin", "Testosterone", "Estrogen", "Beta HCG"
+  ],
+  "Toxicology": [
+    "Drug Screening", "Alcohol Level", "Lead", "Arsenic", "Mercury"
+  ],
+  "Special Chemistry": [
+    "Electrolytes (Na, K, Cl)", "Ammonia", "Lactate", "Iron Studies", "Vitamin B12", "Vitamin D"
+  ]
+};
+
+export default labSections;
