198bfd3d841a33fdaf54507d67c348ac93a62658
Asad4556 <mrasad0101@gmail.com>
2025-08-12 11:19:35 +0500

    Delete assets directory

--- assets/js/get_tests.php
+++
@@ -1,9 +0,0 @@
-<?php
-// assets/js/get_tests.php
-require_once __DIR__ . '/../../config.php';
-$dept = isset($_GET['dept']) ? (int)$_GET['dept'] : 0;
-$stmt = $db->prepare("SELECT id,name FROM tests WHERE department_id = :d LIMIT 200");
-$stmt->execute([':d'=>$dept]);
-$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-header('Content-Type: application/json');
-echo json_encode($rows);
